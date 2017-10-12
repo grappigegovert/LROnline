3e1b4d2697c79a4fc0ecd705acf795aa1226751d
Govert de Gans <grappigegovert@hotmail.com>
2017-10-12 15:02:35 +0200

    Upgrade settings when new version

--- Client/Program.cs
+++ Client/Program.cs
@@ -20,6 +20,13 @@ namespace Client
             //MessageBox.Show("This software is not involved in ANY WAY with the the LEGO company. This means:" + Environment.NewLine + "- This software is not sponsored by the LEGO company in ANY WAY" + Environment.NewLine + "- This software recieves NO SUPPORT from the LEGO company" + Environment.NewLine + "- All the LEGO company copyrighted models, like logos, are NOT owned by the creators of this software and are owned by the LEGO company" + Environment.NewLine + Environment.NewLine + "By using this software, you agree that you will not (try to) make money with this software and agree that this software is in NO WAY involved with the LEGO company.", "Attention");
             //Properties.Settings.Default.GameClientDirectory = "";
             //Properties.Settings.Default.Save();
+	        if (Properties.Settings.Default.NeedsUpdating)
+	        {
+		        Properties.Settings.Default.Upgrade();
+				Properties.Settings.Default.NeedsUpdating = false;
+		        Properties.Settings.Default.Save();
+	        }
+
             if (String.IsNullOrWhiteSpace(Properties.Settings.Default.GameClientDirectory) || Properties.Settings.Default.ForceVersion == "Invalid")
             {
                 Application.Run(new WizardForm());
--- Client/Properties/Settings.Designer.cs
+++ Client/Properties/Settings.Designer.cs
@@ -118,5 +118,17 @@ namespace Client.Properties {
                 this["ForceVersion"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool NeedsUpdating {
+            get {
+                return ((bool)(this["NeedsUpdating"]));
+            }
+            set {
+                this["NeedsUpdating"] = value;
+            }
+        }
     }
 }
